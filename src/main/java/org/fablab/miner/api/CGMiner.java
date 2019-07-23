6dc5e75a7146d98bd87c0fd4ef1092b8cb383e11
fabianpol <s13580@pjwstk.edu.pl>
2019-07-23 07:37:04 +0200

    Introduce Stats

--- src/main/java/org/fablab/miner/api/CGMiner.java
+++ src/main/java/org/fablab/miner/api/CGMiner.java
@@ -18,32 +18,40 @@ public class CGMiner {
 	}
 
 	public Summary summary(Miner miner) {
-		return process(miner, "summary", Summary.class);
+		return parse(miner, "summary", Summary.class);
 	}
 
 	public Pools pools(Miner miner) {
-		return process(miner, "pools", Pools.class);
+		return parse(miner, "pools", Pools.class);
 	}
 
 	public Version version(Miner miner) {
-		return process(miner, "version", Version.class);
+		return parse(miner, "version", Version.class);
 	}
 
 	public Stats stats(Miner miner) {
-		String query = createQuery("stats");
-		String result = JSONSyntaxCorrector.correctSyntax(commandService.process(query, miner));
-		return parseResponse(result, "STATS".toUpperCase(), Stats.class);
+		String result = JSONSyntaxCorrector.correctSyntax(query(miner, "stats"));
+		return parseResponse(result, "STATS", 1, Stats.class);
 	}
 
-	private <T> T process(Miner miner, String command, Class<T> clazz) {
-		String query = createQuery(command);
-		String result = commandService.process(query, miner);
+	private <T> T parse(Miner miner, String command, Class<T> clazz) {
+		String result = query(miner, command);
 		return parseResponse(result, command.toUpperCase(), clazz);
 	}
 
+	private String query(Miner miner, String command) {
+		String query = createQuery(command);
+		String result = commandService.process(query, miner);
+		return result;
+	}
+
 	private <T> T parseResponse(String json, String arrayName, Class<T> type) {
+		return parseResponse(json, arrayName, 0, type);
+	}
+
+	private <T> T parseResponse(String json, String arrayName, int index, Class<T> type) {
 		JSONObject jsonObj = new JSONObject(json);
-		return gson.fromJson(jsonObj.getJSONArray(arrayName).get(0).toString(), type);
+		return gson.fromJson(jsonObj.getJSONArray(arrayName).get(index).toString(), type);
 	}
 
 	private String createQuery(String command) {
--- src/main/java/org/fablab/miner/api/Stats.java
+++ src/main/java/org/fablab/miner/api/Stats.java
@@ -1,5 +1,95 @@
 package org.fablab.miner.api;
 
+import com.google.common.base.MoreObjects;
+import com.google.gson.annotations.SerializedName;
+
 public class Stats {
+
+	@SerializedName("ID")
+	private String id;
+	@SerializedName("Elapsed")
+	private Long elapsed;
+	@SerializedName("Calls")
+	private Integer calls;
+	@SerializedName("Wait")
+	private Double wait;
+	@SerializedName("Max")
+	private Double max;
+	@SerializedName("Min")
+	private Double min;
+	@SerializedName("GHS 5s")
+	private Double ghs5s;
+	@SerializedName("GHS av")
+	private Double ghsAv;
+	@SerializedName("miner_count")
+	private Integer minerCount;
+	@SerializedName("frequency")
+	private Integer frequency;
+	@SerializedName("fan_num")
+	private Integer fanNum;
+	@SerializedName("temp_num")
+	private Integer tempNum;
+	@SerializedName("temp_max")
+	private Integer tempMax;
+
+	public String getId() {
+		return id;
+	}
+
+	public Long getElapsed() {
+		return elapsed;
+	}
+
+	public Integer getCalls() {
+		return calls;
+	}
+
+	public Double getWait() {
+		return wait;
+	}
+
+	public Double getMax() {
+		return max;
+	}
+
+	public Double getMin() {
+		return min;
+	}
+
+	public Double getGhs5s() {
+		return ghs5s;
+	}
+
+	public Double getGhsAv() {
+		return ghsAv;
+	}
+
+	public Integer getMinerCount() {
+		return minerCount;
+	}
+
+	public Integer getFrequency() {
+		return frequency;
+	}
+
+	public Integer getFanNum() {
+		return fanNum;
+	}
+
+	public Integer getTempNum() {
+		return tempNum;
+	}
+
+	public Integer getTempMax() {
+		return tempMax;
+	}
+
+	@Override
+	public String toString() {
+		return MoreObjects.toStringHelper(Stats.class).add("id", id).add("elapsed", elapsed).add("calls", calls)
+				.add("wait", wait).add("max", max).add("min", min).add("ghs 5s", ghs5s).add("ghs av", ghsAv)
+				.add("miner_count", minerCount).add("frequency", frequency).add("fan_num", fanNum)
+				.add("temp_num", tempNum).add("temp_max", tempMax).toString();
+	}
 
 }
