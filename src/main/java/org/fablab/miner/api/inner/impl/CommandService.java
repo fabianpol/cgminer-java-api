277785d38eb0c31a9218432be874ca1d6228d2b5
fabianpol <s13580@pjwstk.edu.pl>
2019-08-12 19:37:55 +0200

    Allow modifing socket timeout by setting system property - cgminer.socket.timeout

--- src/main/java/org/fablab/miner/api/inner/impl/CommandService.java
+++ src/main/java/org/fablab/miner/api/inner/impl/CommandService.java
@@ -5,7 +5,9 @@ import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.io.UncheckedIOException;
 import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.net.SocketAddress;
 import java.net.UnknownHostException;
 import org.fablab.miner.api.Miner;
 import org.slf4j.Logger;
@@ -15,6 +17,8 @@ public class CommandService {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(CommandService.class);
 
+  private static final String socketTimeoutProperty = "cgminer.socket.timeout";
+  private static final int DEFAULT_TIMEOUT = 3000;
   private static final int MAXRECEIVESIZE = 65535;
 
   public String process(String cmd, Miner miner) {
@@ -34,7 +38,10 @@ public class CommandService {
     char buf[] = new char[MAXRECEIVESIZE];
     int len = 0;
     LOGGER.debug("Attempting to run command: {} on {}:{}", cmd, ip, port);
-    try (Socket socket = new Socket(ip, port)) {
+    try (Socket socket = new Socket()) {
+      SocketAddress sockaddr = new InetSocketAddress(ip, port);
+      socket.connect(sockaddr, getTimout());
+      socket.setSoTimeout(getTimout());
       PrintStream ps = new PrintStream(socket.getOutputStream());
       ps.print(cmd.toCharArray());
       ps.flush();
@@ -55,6 +62,14 @@ public class CommandService {
     LOGGER.debug("The miner {}:{} successfully responded", ip, port);
     LOGGER.trace("The miner response: {}", sb.toString());
     return sb.toString();
+  }
+
+  private int getTimout() {
+    String customTimeout = System.getProperty(socketTimeoutProperty);
+    if (customTimeout != null) {
+      return Integer.parseInt(customTimeout);
+    }
+    return DEFAULT_TIMEOUT;
   }
 
 }
